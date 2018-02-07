d1f9598c5a1a49d8d7de21d5a0afbd71a2275e66
tnauss <nauss@staff.uni-marburg.de>
2018-02-07 23:46:40 +0100

    Add first uni

---
+++ docs/_unit01/unit01.md
@@ -0,0 +1,23 @@
+---
+title: Welcome to the real world
+category: unit01
+layout: single
+sidebar:
+  nav: docs
+tags: Unit|01
+---
+
+The following slides will provide you some idea of how one gets from an idea to a research project to some data to many problems.
+
+
+<div class="media-container">
+<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSIfJ5oYvUNSPIKulE4NJzbYam8zEKOFnDrRnXx54xgofHore4CwqJ9iyKqeiG5LpjsNNs_BbjOeY_q/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+</div>
+
+[Download slides](https://docs.google.com/presentation/d/15vc0ox_NfdoAE6KI6sPdSFXqCzqzCA4X5N6ELtdBi8k/export/pdf)
+
+<div class="media-container">
+<video controls autoplay="autoplay" loop="loop" width="936" height="512">
+  <source src="{{site.baseurl}}/assets/videos/guide.mp4" type="video/mp4">
+</video>
+</div>
---
+++ test/assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "Welcome to the real world",
+        "excerpt":"The following slides will provide you some idea of how one gets from an idea to a research project to some data to many problems.Download slides  ","categories": ["unit01"],
+        "tags": ["Unit|01"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit01/unit01.html",
+        "teaser":null},{
+        "title": "02 LN Data Files",
+        "excerpt":"Just a test - taken from jekyllrb.comIn addition to the built-in variables available from Jekyll,you can specify your own custom data that can be accessed via the Liquidtemplating system.Jekyll supports loading data from YAML, JSON,and CSV files located in the _data directory.Note that CSV files must contain a header row.This...","categories": ["unit02"],
+        "tags": ["Unit","02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit02/testdunit02.html",
+        "teaser":null},{
+        "title": "01 LN Data Files",
+        "excerpt":"Just a test - taken from jekyllrb.comIn addition to the built-in variables available from Jekyll,you can specify your own custom data that can be accessed via the Liquidtemplating system.Jekyll supports loading data from YAML, JSON,and CSV files located in the _data directory.Note that CSV files must contain a header row.This...","categories": ["unit02"],
+        "tags": ["Unit","02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit02/testdunit03.html",
+        "teaser":null},{
+        "title": "Units",
+        "excerpt":"Course overviewWelcome to the real worldWelcome to the real world{}","categories": ["units"],
+        "tags": [],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/units/index.html",
+        "teaser":null},]
