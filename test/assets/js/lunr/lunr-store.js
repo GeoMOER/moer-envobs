86874c754a188c3d0c71267d3fcdcb538d0b7b31
tnauss <nauss@staff.uni-marburg.de>
2018-03-03 20:48:53 +0100

    Update

--- test/assets/js/lunr/lunr-store.js
+++ test/assets/js/lunr/lunr-store.js
@@ -1,21 +1,51 @@
 var store = [{
         "title": "Welcome to the real world",
-        "excerpt":"The following slides will provide you some idea of how one gets from an idea to a research project to some data to many problems.Download slides  ","categories": ["unit01"],
+        "excerpt":"Getting from an idea to a (small) research project often begins by chance. A vague idea starts a discussion, one is reminded of related or preliminary own work and sooner or later one decides to try something out. The following slides will illustrate one such example.In order to get a...","categories": ["unit01"],
         "tags": ["Unit|01"],
-        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit01/unit01.html",
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit01/unit01-01.html",
         "teaser":null},{
-        "title": "02 LN Data Files",
-        "excerpt":"Just a test - taken from jekyllrb.comIn addition to the built-in variables available from Jekyll,you can specify your own custom data that can be accessed via the Liquidtemplating system.Jekyll supports loading data from YAML, JSON,and CSV files located in the _data directory.Note that CSV files must contain a header row.This...","categories": ["unit02"],
-        "tags": ["Unit","02"],
-        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit02/testdunit02.html",
+        "title": "DP: Questions and work packages",
+        "excerpt":"Guiding questions and workpackagesBefore jumping directly into planning the analysis workflow, take your time and define some questions, hypothesis and/or aims of your study. This is the most important task in data anlysis and modeling since these questions will guide you through your analysis and ensure - if sticked to...","categories": ["unit01"],
+        "tags": ["Unit|01"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit01/unit01-02.html",
         "teaser":null},{
-        "title": "01 LN Data Files",
-        "excerpt":"Just a test - taken from jekyllrb.comIn addition to the built-in variables available from Jekyll,you can specify your own custom data that can be accessed via the Liquidtemplating system.Jekyll supports loading data from YAML, JSON,and CSV files located in the _data directory.Note that CSV files must contain a header row.This...","categories": ["unit02"],
-        "tags": ["Unit","02"],
-        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit02/testdunit03.html",
+        "title": "Standing on the shoulders of (messy) giants",
+        "excerpt":"Building things from scratch is time consuming, error prone and - thanks to many people distributing their work with open licenses - generally not necessary. Open science is not just a public relation term but strives for a community which works together on a subject and openly shares data, methods...","categories": ["unit02"],
+        "tags": ["Unit|02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit02/unit02-01.html",
+        "teaser":null},{
+        "title": "PA: R Package",
+        "excerpt":"One R package, one script collection and two GitHub repositoriesUsing the readily available scripts as a basis we will create an R package for the more general functionality and an R script collection (e.g. R Studio project or just a folder with scripts) for the more specific tasks. Each will...","categories": ["unit02"],
+        "tags": ["Unit|02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit02/unit02-02.html",
+        "teaser":null},{
+        "title": "PR: R Package Functions",
+        "excerpt":"Implementing an R Package is and keeping track of all things is quite a headache. Hence, we will cross check the functions following a peer review approach.Review package functions and fix issuesPlease review the functions of another peer from the beechForestDynamics R package. If the functions need some enhancement or...","categories": ["unit02"],
+        "tags": ["Unit|02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit02/unit02-03.html",
+        "teaser":null},{
+        "title": "PA: Automatic script",
+        "excerpt":"Automatic processing of MODIS data using the R package functionsNow that a first reviewed version of the beechForestDynamics is at hand, use the functionality to implement a script which handles the MODIS processing automatically following the template provided in project_beech_dynamics and already used as part of Unit02 | PA: R...","categories": ["unit02"],
+        "tags": ["Unit|02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit02/unit02-04.html",
+        "teaser":null},{
+        "title": "Analyzing beech forest dynamics",
+        "excerpt":"You already came up with some guiding questions as part of DP: Questions and work packages. They focus on potential differences between productivity dynamics of primary (Beech) and other forests and the influence/buffering of climatic fluctuations by primary forests. Let’s put this and the processed MODIS data resulting from Unit02...","categories": ["unit03"],
+        "tags": ["Unit|03"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit03/unit03-01.html",
+        "teaser":null},{
+        "title": "DP: Analysis questions",
+        "excerpt":"Given the target area of Romania and keeping the role of spatial forest disturbances in mind, it is time to fine tune the guiding questions for the analysis.Group discussion promptPlease refine your guiding questions from DP: Questions and work packages and focus on the study region of Romania. Come up...","categories": ["unit03"],
+        "tags": ["Unit|03"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit03/unit03-02.html",
+        "teaser":null},{
+        "title": "AT: Analysis Task",
+        "excerpt":"","categories": ["unit03"],
+        "tags": ["Unit|03"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/unit03/unit03-03.html",
         "teaser":null},{
         "title": "Units",
-        "excerpt":"Course overviewWelcome to the real worldWelcome to the real world{}","categories": ["units"],
+        "excerpt":"Unit 01Welcome to the real worldDP: Questions and work packagesUnit 02Standing on the shoulders of (messy) giantsPA: R PackagePR: R Package FunctionsPA: Automatic scriptUnit 03Analyzing beech forest dynamicsDP: Analysis questionsAT: Analysis Task","categories": ["units"],
         "tags": [],
         "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2422/units/index.html",
         "teaser":null},]
